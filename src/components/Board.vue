9c092f960890a7aa9a1b9baaa58a63b52713b72b
Jonathon Bowring <jbowring@informatica.com>
2020-05-28 19:51:08 +1000

    Initial creation of the board and cell components

---
+++ src/components/Board.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="board">
+		<Cell v-for="n in 200" v-bind:key="'cell' + n"/>
+	</div>
+</template>
+
+<script>
+import Cell from './Cell.vue'
+
+export default {
+	name: 'Board',
+    components: {
+      Cell
+    }
+}
+</script>
+
+<style scoped>
+	.board {
+		width: 100%;
+		display: grid;
+		grid-template-columns: repeat(20, 1fr);
+		grid-template-rows: repeat(10, 1fr);
+	}
+</style>
